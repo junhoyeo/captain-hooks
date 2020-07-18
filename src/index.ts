6b0a50d395ee6b46bd8a2adc7ff74dec6fd12d7a
JunhoYeo <hanaro0704@gmail.com>
2020-07-19 02:23:48 +0900

    feat: add useFormattedCountUp hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useSafeWindow } from 'use-safe-window';
+export { default as useFormattedCountUp } from './useFormattedCountUp';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useWindowSize } from './useWindowSize';
